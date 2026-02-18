0b94952f3a526d58abc5c117fb8c376dbee1fe94
Ayush Shrestha <ayush.shrestha517@gmail.com>
2026-02-18 13:03:46 +0545

    added loop for better user experience

--- 02_factorial.py
+++ 02_factorial.py
@@ -1,12 +1,16 @@
 # factorial of a given number
-try:
-    n = int(input("Enter a number: "))
-    if n < 0:
-        print("Factorial is not defined for negative numbers.")
-    else:
-        f = 1
-        for i in range(1,n+1):
-            f *= i
-        print(f"The factorial of {n} is {f}")
-except ValueError:
-    print("Enter a valid integer.")
+while True:
+    try:
+        n = int(input("Enter a number: "))
+        if n < 0:
+            print("Factorial is not defined for negative numbers.")
+        else:
+            f = 1
+            for i in range(1,n+1):
+                f *= i
+            print(f"The factorial of {n} is {f}")
+        ch = input("Do you want to run the program again for a different number?(yes/no) ")
+        if ch.strip().lower() == "no":
+            break
+    except ValueError:
+        print("Enter a valid integer.")
