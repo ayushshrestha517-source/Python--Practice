3f8876cdffd809a090e892c62fd2941ac5bbd31e
Ayush Shrestha <ayush.shrestha517@gmail.com>
2026-02-18 14:07:54 +0545

    new file

---
+++ 03_asterisk_pyramid.py
@@ -0,0 +1,10 @@
+# to display following pattern
+'''
+  *
+ ***
+***** for n =3
+'''
+n = int(input("Enter value for n: "))
+for i in range (1,n+1):
+    print(" "*(n-i),end="")
+    print("*"*(2*i-1))
